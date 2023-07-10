13664325d04eab553c1859562b18e3946a66305d
Vinícius Costa <viniciusccosta95@gmail.com>
2023-07-09 21:42:41 -0300

    Envio em massa dos e-mails com Promise.all. Exclusão do botao de parar. Prettier dos arquivos. Numeração da versão mais condizente.

--- background.js
+++ background.js
@@ -1,78 +1,167 @@
-// --------------------------------------------------------------------------------------------------------
-let running         = false;
-let destinatarios   = [];
-let total_emails    = 0;
-let emails_sent     = 0;
+// ---------------------------------------------------------------------
+var ls_running       = false;
+var ls_total_emails  = 0;
+var ls_emails_sent   = 0;
+var ls_destinatarios = [];
 
-// --------------------------------------------------------------------------------------------------------
-async function save_state(){
-    await messenger.storage.local.set(
-        {
-            running:       running,
-            destinatarios: destinatarios,
-            total_emails:  total_emails,
-            emails_sent:   emails_sent
-        }
-    );
+// ---------------------------------------------------------------------
+async function save_state() {
+    /* Função responsável por salvar o valor atual das variáveis globais no localStorage. */
+
+    await messenger.storage.local.set({
+        running: ls_running,
+        total_emails: ls_total_emails,
+        emails_sent: ls_emails_sent,
+        destinatarios: ls_destinatarios,
+    });
 }
-  
-async function forwardSelectedEmails(dests) {
-    let tabs              = await messenger.tabs.query({ active: true, currentWindow: true });
 
-    let selected_messages = await messenger.mailTabs.getSelectedMessages(tabs[0].id);
-    selected_messages     = selected_messages.messages;
+async function update_state(running, total_emails, emails_sent, destinatarios) {
+    /* Função responsável por atualizar as variáveis globais, conforme parâmetros, e solicitar a atualização no localStorage. */
 
-    running               = true;
-    total_emails          = selected_messages.length;
-    emails_sent           = 0;
-    destinatarios         = dests;
-    await save_state();
-
-    for (let i = 0; i < total_emails; i++){
-        if (!running){
-            console.log("Parando...");
-            break;
-        }
-
-        let message  = selected_messages[i];
-        let account  = await messenger.accounts.get(message.folder.accountId);
-        let identity = account.identities[0];
-
-        let tab_forward = await messenger.compose.beginForward(
-          message.id,
-          "forwardInline",
-          {
-            "to": destinatarios,
-            "from": identity.email,
-            "identityId": identity.id
-          }
-        );
-        
-        // Usar o "onBeforeSend" ?
-        let result = await messenger.compose.sendMessage(tab_forward.id, {"mode": "sendNow"});
-        console.log(result);
-        // Usar o "onAfterSend" ?
-
-        emails_sent += 1;
-        await save_state();
-
-        messenger.runtime.sendMessage({status: "sent"});
+    if (running) {
+        ls_running = running;
     }
-    
-    running = false;
-    await save_state();
+    if (total_emails) {
+        ls_total_emails = total_emails;
+    }
+    if (emails_sent) {
+        ls_emails_sent = emails_sent;
+    }
+    if (destinatarios) {
+        ls_destinatarios = destinatarios;
+    }
 
-    messenger.runtime.sendMessage({status: "done"});
+    await save_state();
 }
 
-// --------------------------------------------------------------------------------------------------------
-messenger.runtime.onMessage.addListener((message) => {
-    if (message.action === "start"){
-        forwardSelectedEmails( message.destinatarios ); // await ?
-    } else if (message.action === "stop"){
-        running = false;
-        messenger.runtime.sendMessage({status: "done"});
+async function send_message_frontend(value) {
+    /* Função responsável por enviar uma mensagem para o FrontEnd */
+    try {
+        await messenger.runtime.sendMessage({ status: value });
+    } catch (error) {
+        // console.log(`Erro ao enviar mensagem ao frontend: |${error}|.`);
+    }
+}
+
+async function getSelectedEmails() {
+    /* Função responsável por recuperar as mensagens selecionadas pelo usuário. */
+
+    let tabs = await messenger.tabs.query({
+        active: true,
+        currentWindow: true,
+    });
+    let selected_messages = await messenger.mailTabs.getSelectedMessages(
+        tabs[0].id
+    );
+
+    selected_messages = selected_messages.messages;
+
+    return selected_messages;
+}
+
+async function forwardEmail(message, email_index) {
+    /* Função responsável por efetivamente encaminhar um e-mail. */
+
+    let promise = new Promise(async (resolve, reject) => {
+        try {
+            // Recupera informações do remetente:
+            let account = await messenger.accounts.get(
+                message.folder.accountId
+            );
+            let identity = account.identities[0];
+
+            // Abre uma janela de encaminhamento de e-mails:
+            let tab_forward = await messenger.compose.beginForward(
+                message.id,
+                "forwardInline",
+                {
+                    to: ls_destinatarios,
+                    from: identity.email,
+                    identityId: identity.id,
+                }
+            );
+
+            // Minimiza a janela:
+            let window = await messenger.windows.get(tab_forward.windowId); // Recupera a janela
+            window = await messenger.windows.update(window.id, {
+                state: "minimized",
+            }); // Minimiza a janela
+
+            // Envia o e-mail:
+            await messenger.compose.sendMessage(tab_forward.id, {
+                mode: "sendNow",
+            }); // Efetivamente envia o e-mail
+
+            // Atualiza variáveis globais e localStorage:
+            await update_state(
+                ls_running,
+                ls_total_emails,
+                ls_emails_sent + 1,
+                ls_destinatarios
+            );
+            console.log(
+                `E-mail ${email_index} enviado com sucesso (${ls_emails_sent}/${ls_total_emails});`
+            );
+
+            // Informa ao frontend:
+            send_message_frontend("sent");
+        } catch (error) {
+            console.log(
+                `Erro ao encaminhar o e-mail ${email_index}: |${error}|;`
+            );
+            reject(error);
+        }
+
+        // Fim da Promisse
+        resolve();
+    });
+
+    return promise;
+}
+
+async function forwardSelectedEmails(destinatarios) {
+    /* Função responsável por coordenar o encaminhamento dos e-mails selecionados. */
+
+    // Recupera os e-mails selecionados:
+    let selected_messages = await getSelectedEmails();
+    // console.log("E-mails selecionados", selected_messages);
+
+    // Atualiza variáveis globais e localStorage:
+    await update_state(true, selected_messages.length, 0, destinatarios);
+
+    // Cria as promises de encaminhamento de e-mail:
+    let promises = selected_messages.map((message, i) =>
+        forwardEmail(message, i)
+    );
+    // console.log("Promises criadas", promises);
+
+    // Executa todas as promises:
+    await Promise.all(promises)
+        .then(() => {
+            console.log("Todos e-mails enviados com sucesso;");
+        })
+        .catch((error) => {
+            console.log(`Erro ao enviar e-mail: |${error}|.`);
+        });
+    // console.log("Promises executadas");
+
+    // Atualiza variáveis globais e localStorage:
+    await update_state(
+        false,
+        ls_total_emails,
+        ls_emails_sent,
+        ls_destinatarios
+    );
+    await send_message_frontend("done");
+    // console.log("Fim do forwardSelectedEmails");
+}
+
+// ---------------------------------------------------------------------
+messenger.runtime.onMessage.addListener(async (message) => {
+    if (message.action === "start") {
+        await forwardSelectedEmails(message.destinatarios);
     }
 });
 
-// --------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------
--- popup/popup.js
+++ popup/popup.js
@@ -8,236 +8,229 @@ let emails_sent   = 0;
 const email_regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 // --------------------------------------------------------------------------------------------------------
-async function update_variables(){
-  let ls = await messenger.storage.local.get(
-    {
-      running:       false,
-      destinatarios: [],
-      total_emails:  [],
-      emails_sent:   0
-    }
-  );
+async function update_variables() {
+    let ls = await messenger.storage.local.get({
+        running: false,
+        destinatarios: [],
+        total_emails: [],
+        emails_sent: 0,
+    });
 
-  running       = ls.running;
-  total_emails  = ls.total_emails;
-  destinatarios = ls.destinatarios;
-  emails_sent   = ls.emails_sent;
+    running       = ls.running;
+    total_emails  = ls.total_emails;
+    destinatarios = ls.destinatarios;
+    emails_sent   = ls.emails_sent;
 }
 
-async function init_popup(){
-  await update_variables();
+async function init_popup() {
+    await update_variables();
 
-  if (running){
-    freeze();
-  }
+    if (running) {
+        freeze();
+    }
 
-  update_progress();
-  update_table();
+    update_progress();
+    update_table();
 
-  if (destinatarios.length > 0){
-    document.getElementById("btn_start").disabled = false;
-  }
+    if (destinatarios.length > 0) {
+        document.getElementById("btn_start").disabled = false;
+    }
 }
 
 // --------------------------------------------------------------------------------------------------------
 function new_uuid() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
-
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (
+            c ^
+            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+    );
 }
 
-function addDestinatario(d){
-  let uuid = new_uuid();
+function addDestinatario(d) {
+    let uuid = new_uuid();
 
-  let tbody = document.getElementById("lista");
+    let tbody = document.getElementById("lista");
 
-  let tr = document.createElement("tr");
-  tr.setAttribute("data-id", uuid);
+    let tr = document.createElement("tr");
+    tr.setAttribute("data-id", uuid);
 
-  let td1 = document.createElement("td");
-  td1.innerText = d;
-  tr.appendChild(td1);
+    let td1 = document.createElement("td");
+    td1.innerText = d;
+    tr.appendChild(td1);
 
-  let td2 = document.createElement("td");
-  let btn   = document.createElement("button");
-  let i     = document.createElement("i");
-  i.classList.add('bx', 'bx-trash-alt');
-  btn.appendChild(i);
-  td2.appendChild(btn);
+    let td2 = document.createElement("td");
+    let btn = document.createElement("button");
+    let i = document.createElement("i");
+    i.classList.add("bx", "bx-trash-alt");
+    btn.appendChild(i);
+    td2.appendChild(btn);
 
-  tr.appendChild(td2);
+    tr.appendChild(td2);
 
-  tbody.appendChild(tr);
+    tbody.appendChild(tr);
 
-  btn.addEventListener('click', () => delDestinatario(uuid));
+    btn.addEventListener("click", () => delDestinatario(uuid));
 }
 
-async function delDestinatario(uuid){
-  // https://stackoverflow.com/a/66908123
+async function delDestinatario(uuid) {
+    // https://stackoverflow.com/a/66908123
 
-  let table               = document.getElementById("lista");
-  let elements            = Array.from(table.querySelectorAll("tr"));
-  let element_to_delete   = elements.find(el => el.dataset.id === uuid);
-  let destinatario        = element_to_delete.querySelector("td:first-child").innerText;
-  
-  let ls = await messenger.storage.local.get({destinatarios: []});
-  destinatarios = ls.destinatarios;
-  destinatarios = destinatarios.filter(item => item != destinatario);
-  await messenger.storage.local.set({destinatarios: destinatarios});
+    let table = document.getElementById("lista");
+    let elements = Array.from(table.querySelectorAll("tr"));
+    let element_to_delete = elements.find((el) => el.dataset.id === uuid);
+    let destinatario =
+        element_to_delete.querySelector("td:first-child").innerText;
 
-  table.removeChild(element_to_delete);
+    let ls = await messenger.storage.local.get({ destinatarios: [] });
+    destinatarios = ls.destinatarios;
+    destinatarios = destinatarios.filter((item) => item != destinatario);
+    await messenger.storage.local.set({ destinatarios: destinatarios });
 
-  if (destinatarios.length == 0){
-    document.getElementById("btn_start").disabled = true;
-  }
-}
+    table.removeChild(element_to_delete);
 
-function validateDestinatario(destinatario){
-  let tbody = document.getElementById("lista");
-  let destinatarios = tbody.querySelectorAll("tr > td:first-child")
-  
-
-  // Verifica se é um e-mail válido
-  if (!email_regex.test(destinatario)){
-    return false;
-  }
-
-  // Verificando se é único:
-  for (let i = 0; i < destinatarios.length; i++){
-    if (destinatarios[i].innerText === destinatario){
-      return false;
+    if (destinatarios.length == 0) {
+        document.getElementById("btn_start").disabled = true;
     }
-  }
-
-
-  return true;
 }
 
-function getDestinatarios(){
-  let tbody = document.getElementById("lista");
-  let tds = tbody.querySelectorAll("tr > td:first-child");
-  
-  let destinatarios = [];
-  for (let i = 0; i < tds.length; i++){
-    destinatarios.push(tds[i].innerText);
-  }
-  
-  return destinatarios;
-}
+function validateDestinatario(destinatario) {
+    let tbody = document.getElementById("lista");
+    let destinatarios = tbody.querySelectorAll("tr > td:first-child");
 
-function update_progress(){
-  let bar = document.getElementById("bar");
-  let txt = document.getElementById("progress_text");
-
-  if (total_emails > 0){
-    let width = parseInt( ( emails_sent / total_emails ) * 100);
-
-    if (width <= 100){
-      bar.style.width = width + "%";
-      progress_text.innerHTML = `Enviado ${emails_sent} de ${total_emails}`;
+    // Verifica se é um e-mail válido
+    if (!email_regex.test(destinatario)) {
+        return false;
     }
-  } else {
-    bar.style.width = "0%";
-  }
-  
+
+    // Verificando se é único:
+    for (let i = 0; i < destinatarios.length; i++) {
+        if (destinatarios[i].innerText === destinatario) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-function update_table(){
-  for (let i = 0; i < destinatarios.length; i++){
-    let destinatario = destinatarios[i];
-    addDestinatario(destinatario);
-  }
+function getDestinatarios() {
+    let tbody = document.getElementById("lista");
+    let tds = tbody.querySelectorAll("tr > td:first-child");
+
+    let destinatarios = [];
+    for (let i = 0; i < tds.length; i++) {
+        destinatarios.push(tds[i].innerText);
+    }
+
+    return destinatarios;
 }
 
-function freeze(){
-  let input     = document.getElementById("input");
-  let btn_start = document.getElementById("btn_start");
-  let btn_stop  = document.getElementById("btn_stop");
+function update_progress() {
+    let bar = document.getElementById("bar");
+    let txt = document.getElementById("progress_text");
 
-  table_btns    = document.querySelectorAll("tbody > tr > td:nth-child(2)");
-  for (let i = 0; i < table_btns.length; i++){
-    table_btns[i].disabled = true;
-  }
+    if (total_emails > 0) {
+        let width = parseInt((emails_sent / total_emails) * 100);
 
-  input.disabled      = true;
-  btn_start.disabled  = true;
-  btn_stop.disabled   = false;
+        if (width <= 100) {
+            bar.style.width = width + "%";
+            progress_text.innerHTML = `Enviado ${emails_sent} de ${total_emails}`;
+        }
+    } else {
+        bar.style.width = "0%";
+    }
 }
 
-function unfreeze(){
-  let input     = document.getElementById("input");
-  let btn_start = document.getElementById("btn_start");
-  let btn_stop  = document.getElementById("btn_stop");
+function update_table() {
+    for (let i = 0; i < destinatarios.length; i++) {
+        let destinatario = destinatarios[i];
+        addDestinatario(destinatario);
+    }
+}
 
-  table_btns    = document.querySelectorAll("tbody > tr > td:nth-child(2)");
-  for (let i = 0; i < table_btns.length; i++){
-    table_btns[i].disabled = false;
-  }
+function freeze() {
+    let input = document.getElementById("input");
+    let btn_start = document.getElementById("btn_start");
+    // let btn_stop = document.getElementById("btn_stop");
 
-  input.disabled      = false;
-  btn_start.disabled  = false;
-  btn_stop.disabled   = true;
+    table_btns = document.querySelectorAll("tbody > tr > td:nth-child(2)");
+    for (let i = 0; i < table_btns.length; i++) {
+        table_btns[i].disabled = true;
+    }
+
+    input.disabled = true;
+    btn_start.disabled = true;
+    // btn_stop.disabled = false;
+}
+
+function unfreeze() {
+    let input = document.getElementById("input");
+    let btn_start = document.getElementById("btn_start");
+    // let btn_stop = document.getElementById("btn_stop");
+
+    table_btns = document.querySelectorAll("tbody > tr > td:nth-child(2)");
+    for (let i = 0; i < table_btns.length; i++) {
+        table_btns[i].disabled = false;
+    }
+
+    input.disabled = false;
+    btn_start.disabled = false;
+    // btn_stop.disabled = true;
 }
 
 // --------------------------------------------------------------------------------------------------------
-document.getElementById("btn_start").addEventListener('click', (event) => {
-  freeze();
-  dests = getDestinatarios();
+document.getElementById("btn_start").addEventListener("click", (event) => {
+    freeze();
 
-  document.getElementById("progress_text").innerHTML = `Enviando...`;
+    dests = getDestinatarios();
 
-  messenger.runtime.sendMessage( {action:"start", destinatarios: dests} );
+    document.getElementById("progress_text").innerHTML = `Enviando...`;
+
+    messenger.runtime.sendMessage({ action: "start", destinatarios: dests });
 });
 
-document.getElementById("btn_stop").addEventListener('click', (event) => {
-  messeger.runtime.sendMessage({action: "stop"});
-});
+document.getElementById("input").addEventListener("keydown", async (event) => {
+    if (event.key == "Enter") {
+        let destinatario = event.target.value;
 
-document.getElementById("input").addEventListener('keydown', async (event)=>{
-  
-  if (event.key == "Enter"){
-    let destinatario = event.target.value;
+        if (destinatario !== "") {
+            destinatario = destinatario.trim();
 
-    if (destinatario !== ""){
-      destinatario = destinatario.trim();
+            if (!validateDestinatario(destinatario)) {
+                document.getElementById("alert").hidden = false;
+                return;
+            }
 
-      if (!validateDestinatario(destinatario)){
-        document.getElementById("alert").hidden = false;
-        return
-      }
-      
-      let ls = await messenger.storage.local.get({destinatarios: []});
-      destinatarios = ls.destinatarios;
-      destinatarios.push(destinatario);
-      await messenger.storage.local.set({destinatarios: destinatarios});
+            let ls = await messenger.storage.local.get({ destinatarios: [] });
+            destinatarios = ls.destinatarios;
+            destinatarios.push(destinatario);
+            await messenger.storage.local.set({ destinatarios: destinatarios });
 
-      addDestinatario(destinatario);
-      event.target.value = '';
+            addDestinatario(destinatario);
+            event.target.value = "";
 
-      if (destinatarios.length > 0){
-        document.getElementById("btn_start").disabled = false;
-      }
+            if (destinatarios.length > 0) {
+                document.getElementById("btn_start").disabled = false;
+            }
+        }
     }
-  }
 
-  document.getElementById("alert").hidden = true;
+    document.getElementById("alert").hidden = true;
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  init_popup();
+document.addEventListener("DOMContentLoaded", function () {
+    init_popup();
 });
 
 messenger.runtime.onMessage.addListener(async (message) => {
-  await update_variables();
+    await update_variables();
 
-  if (message.status === "done"){
-    update_progress();
-    unfreeze();
-  } else if (message.status == "sent"){
-    update_progress();
-  }
-
+    if (message.status === "done") {
+        update_progress();
+        unfreeze();
+    } else if (message.status == "sent") {
+        update_progress();
+    }
 });
 
 // --------------------------------------------------------------------------------------------------------
